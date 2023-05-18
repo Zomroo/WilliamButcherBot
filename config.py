412b501d8b66ec8c74dd3bbb2a9b0f867689235b
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-18 19:04:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ LOG_GROUP_ID = int(os.environ.get('LOG_GROUP_ID', -1001646246141))
 GBAN_LOG_GROUP_ID = int(os.environ.get('GBAN_LOG_GROUP_ID', -1001646246141))
 MESSAGE_DUMP_CHAT = int(os.environ.get('MESSAGE_DUMP_CHAT', -1001646246141))
 WELCOME_DELAY_KICK_SEC = int(os.environ.get('WELCOME_DELAY_KICK_SEC', 600))
-MONGO_URL = os.environ.get('mongodb+srv://Zoro:Zoro@cluster0.x1vigdr.mongodb.net/?retryWrites=true&w=majority')
+MONGO_URL = os.environ.get('mongodb+srv://Ai:Ai@cluster0.892cnzo.mongodb.net/?retryWrites=true&w=majority')
 ARQ_API_KEY = os.environ.get('JEQHPA-MRCARX-SUEPKY-DSXOAP-ARQ')
 ARQ_API_URL = os.environ.get('ARQ_API_URL', 'https://arq.hamker.in')
 LOG_MENTIONS = os.environ.get('LOG_MENTIONS', 'True').lower() in ['true', '1']
